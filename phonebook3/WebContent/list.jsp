b8a62ae741537ec7446e979f320b183ede7904ac
sonddokki <sonsharo@naver.com>
2023-09-06 11:19:10 +0900

    0906 폰북 모델3 수업진행중

---
+++ phonebook3/WebContent/list.jsp
@@ -0,0 +1,69 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page import="com.javaex.dao.PersonDao" %>
+<%@ page import="com.javaex.vo.PersonVo" %>
+<%@ page import="java.util.List" %>
+    
+ <%
+ 	// request의 어트리뷰트 영역에 있는 data를 꺼내온다.
+ 	List<PersonVo> personList = (List<PersonVo>)request.getAttribute("personList");
+ 
+ %>
+ 
+ 
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+
+<h1>전화번호부</h1>
+	<h2>리스트</h2>
+	
+	<p>등록된 전화번호 리스트 입니다.<p>
+	<%for(int i=0;i<personList.size();i++) {%>
+	
+		<table border="1">
+			<%--<tr>
+				<td>코드</td> <td><%=personList.get(i).getPersonId()%></td>			
+			</tr> --%> 
+			<tr>
+				<td>이름(name)</td> <td><%=personList.get(i).getName()%></td>			
+			</tr>
+			<tr>
+				<td>핸드폰(hp)</td> <td><%=personList.get(i).getHp()%></td>		
+			</tr>
+			<tr>
+				<td>회사(company)</td> <td><%=personList.get(i).getCompany()%></td>			
+			</tr>
+			
+			<tr>
+				<td> <a href="">[수정]</td>
+				<td> <a href="">[삭제]</a> </td>	
+			</tr>
+			
+		</table>
+		<br>
+	
+	<% }%>
+	
+	
+	<%-- <a href="http://localhost:8000/phonebook2/writeForm.jsp">전화번호 등록품</a> --%>
+	<a href="./writeForm.jsp">전화번호 등록품</a>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
--- phonebook3/src/main/java/com/javaex/controller/PhonebookController.java
+++ phonebook3/src/main/java/com/javaex/controller/PhonebookController.java
@@ -1,26 +1,48 @@
 package com.javaex.controller;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.javaex.dao.PersonDao;
+import com.javaex.vo.PersonVo;
+
 @WebServlet("/PhonebookController")
 public class PhonebookController extends HttpServlet {
+
 	private static final long serialVersionUID = 1L;
-       
 
+	// Get방식 요청이 들어왔을때 실행되는 메소드
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("PhonebookController");
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 리스트
+		// 1.dao를 통해서 전체 리스트데이터 가져오기
+
+		PersonDao personDao = new PersonDao();
+		List<PersonVo> personList = personDao.personSelect("");
 		
+		System.out.println(personList);
+		
+		// 리스트 화면(html) + date 응답을 해야됨
+		// request data를 넣는다
+		request.setAttribute("personList", personList);
+		
+		// list.jsp 에게 시킨다 (포워드)		
+		RequestDispatcher rd = request.getRequestDispatcher("/list.jsp"); // jsp파일 위치를 적어넣어야함
+		rd.forward(request, response);
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	// Post방식 요청이 들어왔을때 실행되는 메소드
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
